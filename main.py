85370ed0f0672ef56343ad0c83d7905e000953a0
Haruki Shimizu <shumizu418128@gmail.com>
2025-10-05 16:56:35 +0900

    母音・子音・パッチムの辞書をconfigから参照するように変更

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 
 import hgtk
 
+import config
+
 # 例: 삼겹살（サムギョプサル）
 # 例: 딸기（タルギ、いちご） 濃音あり
 # 例: 꽁치（コンチ、サンマ） 濃音あり
@@ -12,119 +14,6 @@ import hgtk
 
 input_text = input("Enter the text: ").strip()
 
-# 母音
-vowel_read_dict = {
-    "ㅏ": "a",
-    "ㅑ": "ya",
-    "ㅓ": "eo (o)",
-    "ㅕ": "yeo (yo)",
-    "ㅗ": "o",
-    "ㅛ": "yo",
-    "ㅜ": "u",
-    "ㅠ": "yu",
-    "ㅡ": "eu (u)",
-    "ㅣ": "i",
-    "ㅔ": "e",  # 本来合成母音だが、合成母音辞書の中でこの文字が出てくるのでここにも書く
-    "ㅐ": "e",  # 本来合成母音だが、合成母音辞書の中でこの文字が出てくるのでここにも書く
-}
-
-# 子音
-consonant_read_dict = {
-    "ㄱ": "k/g",
-    "ㄴ": "n",
-    "ㄷ": "t/d",
-    "ㄹ": "l",
-    "ㅁ": "m",
-    "ㅂ": "p/b",
-    "ㅅ": "s",
-    "ㅇ": "",
-    "ㅈ": "ch/j",
-    "ㅊ": "ch",  # 激音
-    "ㅋ": "k",  # 激音
-    "ㅌ": "t",  # 激音
-    "ㅍ": "p",  # 激音
-    "ㅎ": "h",
-}
-
-# 子音の濃音
-strong_consonant_read_dict = {
-    "ㄲ": ["kk", "ㄱ"],
-    "ㄸ": ["tt", "ㄷ"],
-    "ㅃ": ["pp", "ㅂ"],
-    "ㅆ": ["ss", "ㅅ"],
-    "ㅉ": ["cch", "ㅈ"],
-}
-
-# パッチム
-patchum_read_dict = {
-    "ㄱ": "k",
-    "ㅋ": "k",
-    "ㄲ": "k",
-    "ㄴ": "n",
-    "ㅁ": "m",
-    "ㅇ": "ng",
-    "ㅂ": "p",
-    "ㅍ": "p",
-    "ㄹ": "l",
-    "ㄷ": "ッ",
-    "ㅅ": "ッ",
-    "ㅈ": "ッ",
-    "ㅊ": "ッ",
-    "ㅌ": "ッ",
-    "ㅎ": "ッ",
-    "ㅆ": "ッ",
-}
-
-# 合成母音を分解
-diphthong_read_dict = {
-    "ㅐ": {
-        "symbol": ["ㅏ", "ㅣ"],
-        "reading": "e",
-    },
-    "ㅔ": {
-        "symbol": ["ㅓ", "ㅣ"],
-        "reading": "e",
-    },
-    "ㅒ": {
-        "symbol": ["ㅑ", "ㅣ"],
-        "reading": "ye",
-    },
-    "ㅖ": {
-        "symbol": ["ㅕ", "ㅣ"],
-        "reading": "ye",
-    },
-    "ㅘ": {
-        "symbol": ["ㅗ", "ㅏ"],
-        "reading": "wa",
-    },
-    "ㅝ": {
-        "symbol": ["ㅜ", "ㅓ"],
-        "reading": "wo",
-    },
-    "ㅞ": {
-        "symbol": ["ㅜ", "ㅔ"],
-        "reading": "we",
-        "note": "ㅔも合成母音",
-    },
-    "ㅙ": {
-        "symbol": ["ㅗ", "ㅐ"],
-        "reading": "we",
-        "note": "ㅐも合成母音",
-    },
-    "ㅚ": {
-        "symbol": ["ㅗ", "ㅣ"],
-        "reading": "we",
-        "note": "本来 o + i だが、例外としてweになる",
-    },
-    "ㅟ": {
-        "symbol": ["ㅜ", "ㅣ"],
-        "reading": "wi",
-    },
-    "ㅢ": {
-        "symbol": ["ㅡ", "ㅣ"],
-        "reading": "wi",
-    },
-}
 
 all_result = []
 
@@ -142,7 +31,7 @@ for char in input_text:
 
     # 子音の分析（ネスト構造）
     # 子音の濃音の場合は、詳細情報を追加
-    strong_consonant = strong_consonant_read_dict.get(consonant)
+    strong_consonant = config.strong_consonant_read_dict.get(consonant)
     if strong_consonant:
         consonant_info = {
             "symbol": consonant,
@@ -150,13 +39,13 @@ for char in input_text:
             "reading": strong_consonant[0],
             "base": {
                 "symbol": strong_consonant[1],
-                "reading": consonant_read_dict[strong_consonant[1]],
+                "reading": config.consonant_read_dict[strong_consonant[1]],
             },
         }
     else:
         consonant_info = {
             "symbol": consonant,
-            "reading": consonant_read_dict[consonant],
+            "reading": config.consonant_read_dict[consonant],
         }
     result["components"]["子音"] = consonant_info
 
@@ -165,30 +54,30 @@ for char in input_text:
         "symbol": vowel,
     }
     # 合成母音の場合は2つの母音を分析
-    diphthong = diphthong_read_dict.get(vowel)
+    diphthong = config.diphthong_read_dict.get(vowel)
     if diphthong:
         vowel_info["components"] = [
             {
                 "symbol": diphthong["symbol"][0],
-                "reading": vowel_read_dict[diphthong["symbol"][0]],
+                "reading": config.vowel_read_dict[diphthong["symbol"][0]],
             },
             {
                 "symbol": diphthong["symbol"][1],
-                "reading": vowel_read_dict[diphthong["symbol"][1]],
+                "reading": config.vowel_read_dict[diphthong["symbol"][1]],
             },
         ]
         vowel_info["combined_reading"] = diphthong["reading"]
         vowel_info["note"] = "合成母音"
         vowel_info["note"] += f" ※{diphthong.get('note', '')}"
     else:
-        vowel_info["reading"] = vowel_read_dict[vowel]
+        vowel_info["reading"] = config.vowel_read_dict[vowel]
     result["components"]["母音"] = vowel_info
 
     # パッチムの分析（ネスト構造）
     if patchum:
         patchum_info = {
             "symbol": patchum,
-            "reading": patchum_read_dict.get(patchum, ""),
+            "reading": config.patchum_read_dict.get(patchum, ""),
         }
         result["components"]["パッチム"] = patchum_info
 
